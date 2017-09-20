596ac150696069e1f99de09712bcf08cb58262db
Marc Andrew <hello@marcandrew.net>
2017-09-20 22:52:07 +0100

    Files

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import '../css/main.scss';
+
+import { RandomGenerator } from './random-generator';
+
+const outputParagraph = document.querySelector('#outputParagraph');
+
+const outputRandomInit = () => {
+	outputParagraph.textContent = RandomGenerator.randomInteger();
+};
+
+const outputRandomRange = () => {
+	outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
+};
+
+const buttonRndInt = document.querySelector('#randomInit');
+const buttonRndRange = document.querySelector('#randomRange');
+
+buttonRndInt.addEventListener('click', outputRandomInit);
+buttonRndRange.addEventListener('click', outputRandomRange);
