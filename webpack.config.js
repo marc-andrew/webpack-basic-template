febde67b1c77dd2271a676e9ca63cdbfb51f5732
Marc Andrew <hello@marcandrew.net>
2017-09-21 23:17:04 +0100

    Added more plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,64 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-    filename: 'main.css'
+	filename: 'main.css'
 });
 
+var htmlPlugin = new HtmlWebpackPlugin({
+	template: 'src/index.html'
+});
+
+var cleanPlugin = new CleanWebpackPlugin(['dist']);
+
 module.exports = {
-     entry: './src/js/app.js',
-     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist'
-     },
-     module: {
-         rules: [
-             {
-                 test: /\.js$/,
-                 use: [
-                     {
-                         loader: 'babel-loader',
-                         options: {
-                             presets: ['es2015']
-                         }
-                     }
-                 ]
-             },
-             {
-                 test:  /\.scss$/,
-                 use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                 })
-             }
-         ]
-     },
-     plugins: [
-         extractPlugin
-     ]
- }
+	entry: './src/js/app.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015']
+						}
+					}
+				]
+			},
+			{
+				test:  /\.scss$/,
+				use: extractPlugin.extract({
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							output: 'img/'
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		extractPlugin,
+		htmlPlugin,
+		cleanPlugin
+	]
+}
